7538f4705ebcd8097e62ca529fb7d3c3d4a49c61
BugArgos <128261248+BugArgos@users.noreply.github.com>
2023-09-06 07:58:09 -0300

    Atualizar o Horas.c

--- Horas.c
+++ Horas.c
@@ -7,22 +7,21 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    float horas;
-    int hora, min;
-    int i = 0;
-    while (i == 0){
-        printf("Informe a hora:");
-        scanf("%f", &horas);
-        hora = (int)horas;
-        min = (int)(round((horas - hora) * 100));
-        if ( hora < 0 || hora >23 || min > 59){
-            printf("Formato de número invalido\n");
-            printf("TENTE NOVAMENTE COM OUTROS VALORES\n");
-        }
-        else{
-            printf("%.2f equivale a %d horas e %d minutos\n", horas, hora, min);
-            i = 1;
-        }
+    float hora;
+    int horas, minutos, segundos;
+
+    printf("Digite uma hora no formato (exemplo: 15.35): ");
+    scanf("%f", &hora);
+
+    horas = (int)hora;
+    minutos = (hora - horas) * 100;
+    segundos = minutos * 60;
+
+    if (horas >= 0 && horas <= 23 && minutos >= 0 && minutos <= 59) {
+        printf("Equivalente a %d minutos e %d segundos.\n", minutos, segundos);
+    } else {
+        printf("Hora inválida. Certifique-se de que as horas estão entre 0 e 23 e os minutos entre 0 e 59.\n");
     }
+
     return 0;
 }
